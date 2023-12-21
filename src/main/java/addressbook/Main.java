cd65a991b5518b6b9ab66e8c15d75721afe55907
Shrishti Shah <shubhasritadepalli@gmail.com>
2023-12-21 20:57:37 +0530

    Day5 assignment

---
+++ src/main/java/addressbook/Main.java
@@ -0,0 +1,7 @@
+package addressbook;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("..................Welcome to Address Book...................");
+    }
+}
